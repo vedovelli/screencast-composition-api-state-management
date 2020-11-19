c828744cdbe1161fe7c5439a87125fcbbf7090a2
Fabio Vedovelli <vedovelli@gmail.com>
2020-11-19 10:23:22 +0100

    Ready, set, go!

--- src/store/store.spec.js
+++ src/store/store.spec.js
@@ -13,10 +13,24 @@ describe('Store', () => {
   });
 
   it.todo('should return state.open equals false');
+
+  it.todo('should return state.products equals empty array');
+
+  it.todo('should NOT allow mutate state');
+
   it.todo('should toggle state.open');
+
   it.todo('should add products and open the cart');
+
+  it.todo('should throw an error if no product is provided to actions.add()');
+
   it.todo('should NOT add same product twice');
+
   it.todo('should remove products');
+
+  it.todo('should throw an error if no product is provided to actions.remove()');
+
   it.todo('should keep products if provided product is not found');
+
   it.todo('should remove all products');
 });
